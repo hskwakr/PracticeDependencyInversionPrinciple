7633df8b0c0f31659086612b20e30b421cd55897
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-12-13 22:57:12 +0900

    Add codes of AfterDIP

---
+++ PracticeDependencyInversionPrinciple/Sample1/AfterDIP/EmployeeBusinessLogic.cs
@@ -0,0 +1,17 @@
+﻿namespace PracticeDependencyInversionPrinciple.Sample1.BeforeDIP
+{
+    public class EmployeeBusinessLogic
+    {
+        private EmployeeDataAccess _employeeDataAccess;
+
+        public EmployeeBusinessLogic()
+        {
+            _employeeDataAccess = DataAccessFactory.GetEmployeeDataAccessObj();
+        }
+
+        public Employee GetEmployeeDetails(int id)
+        {
+            return _employeeDataAccess.GetEmployeeDetails(id);
+        }
+    }
+}
